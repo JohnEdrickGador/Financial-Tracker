b8e243e2d722ecaee3b6191c1e5ad69c70ca6524
JohnEdrickGador <jmgador@up.edu.ph>
2025-11-09 01:02:29 +0800

    Add: Installed ExpressJS and SQLite3 to the backend folder for backend development

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+// Initialize expressJS server
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -5,6 +6,16 @@ const PORT = 8080;
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+
+// Start connection with the database
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./financial-tracker.db", (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Connected to the SQLite3 database");
+  }
+});
 
 // CREATE new transaction
 app.post("/transactions/new", (req, res) => {});
@@ -17,3 +28,15 @@ app.put("/transactions/update/:id", (req, res) => {});
 
 // DELETE a transaction
 app.delete("/transactions/delete/:id", (req, res) => {});
+
+// CREATE new bank
+app.post("/banks/new", (req, res) => {});
+
+// READ banks
+app.get("/banks/list", (req, res) => {});
+
+// UPDATE bank details
+app.put("/banks/update/:bank_id", (req, res) => {});
+
+// DELETE a bank
+app.delete("/bank/delete/:id", (req, res) => {});
